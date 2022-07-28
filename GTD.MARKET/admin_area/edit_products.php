37c6128f66ed7bb40cfc53407722d5ec25fb9309
Arteum Galkin <artem.galkin2020@mail.ru>
2022-07-28 16:53:38 +0300

    E-commerce website using PHP and MySQL || Admin-Fetching form data || Part -67

--- GTD.MARKET/admin_area/edit_products.php
+++ GTD.MARKET/admin_area/edit_products.php
@@ -1,29 +1,84 @@
+<?php
+if (isset($_GET['edit_products'])) {
+  $edit_id = $GET['edit_products'];
+  // echo $edit_id;
+  $get_data = "Select * from `products` where product_id=$edit_id";
+  $result = mysqli_query($con, $get_data);
+  $row = mysqli_fetch_assoc($result);
+  $product_title = $row['product_title'];
+  // echo $product_title;
+  $product_description = $row['product_description'];
+  $product_keywords = $row['product_keywords'];
+  $category_id = $row['category_id'];
+  $brand_id = $row['brand_id'];
+  $product_image1 = $row['product_image1'];
+  $product_image2 = $row['product_image2'];
+  $product_image3 = $row['product_image3'];
+  $product_price = $row['product_price'];
+
+
+
+
+  // fetching category name
+  $select_category = "Select * from `categories` where category_id=$category_id";
+  $result_category = mysqli_query($con, $select_category);
+  $row_category = mysqli_fetch_assoc($result_category);
+  $category_title = $row_category['category_title'];
+  // echo $category_title;
+
+
+
+
+
+  // fetching category name
+  $select_brand = "Select * from `brands` where brand_id=$brand_id";
+  $result_brand = mysqli_query($con, $select_brand);
+  $row_brand = mysqli_fetch_assoc($result_brand);
+  $brand_title = $row_brand['brand_title'];
+  // echo $brand_title;
+}
+
+
+?>
+
+
+
+
 <div class="container mt-5">
   <h1 class="text-center">Edit Product</h1>
   <form action="" method="post" enctype="multipart/form-data">
     <div class="form-outline w-50 m-auto mb-4">
       <label for="product_title" class="form-label">Product Title</label>
-      <input type="text" id="product_title" name="product_title" class="form-control" required="required">
+      <input type="text" id="product_title" value="<?php echo $product_title ?>" name="product_title" class="form-control" required="required">
     </div>
 
     <div class="form-outline w-50 m-auto mb-4">
       <label for="product_desc" class="form-label">Product Description</label>
-      <input type="text" id="product_desc" name="product_desc" class="form-control" required="required">
+      <input type="text" id="product_desc" name="product_desc" value="<?php echo $product_description ?>" class="form-control" required="required">
     </div>
 
     <div class="form-outline w-50 m-auto mb-4">
       <label for="product_keywords" class=" form-label">Product Keywords</label>
-      <input type="text" id="product_keywords" name="product_keywords" class="form-control" required="required">
+      <input type="text" id="product_keywords" name="product_keywords" value="<?php echo $product_keywords ?>" class="form-control" required="required">
     </div>
 
     <div class="form-outline w-50 m-auto mb-4">
       <label for="product_category" class=" form-label">Product Categories</label>
       <select name="product_category" class="form-select">
-        <option value="">1</option>
-        <option value="">2</option>
-        <option value="">3</option>
-        <option value="">4</option>
-        <option value="">5</option>
+        <option value="<?php echo $category_title ?>"><?php echo $category_title ?></option>
+        <?php
+
+        $select_category_all = "Select * from `categories`";
+        $result_category_all = mysqli_query($con, $select_category_all);
+        while ($row_category_all = mysqli_fetch_assoc($result_category_all)) {
+          $category_title = $row_category_all['category_title'];
+          $category_id = $row_category_all['category_id'];
+          echo "<option value='$category_id'>$category_title</option>";
+        };
+
+        ?>
+
+
 
       </select>
     </div>
@@ -31,20 +86,27 @@
     <div class="form-outline w-50 m-auto mb-4">
       <label for="product_brands" class=" form-label">Product Brands</label>
       <select name="product_brands" class="form-select">
-        <option value="">1</option>
-        <option value="">2</option>
-        <option value="">3</option>
-        <option value="">4</option>
-        <option value="">5</option>
+
+        <option value="<?php echo $brand_title ?>"><?php echo $brand_title ?></option>
+        <?php
+        $select_brand_all = "Select * from `brands`";
+        $result_brand_all = mysqli_query($con, $select_brand_all);
+        while ($row_brand_all = mysqli_fetch_assoc($result_brand_all)) {
+          $brand_title = $row_brand_all['brand_title'];
+          $brand_id = $row_brand_all['brand_id'];
+          echo "<option value='$brand_id'>$brand_title</option>";
+        };
+        ?>
+
 
       </select>
     </div>
 
     <div class="form-outline w-50 m-auto mb-4">
-      <label for="product_image2" class=" form-label">Product Image1</label>
+      <label for="product_image1" class=" form-label">Product Image1</label>
       <div class="d-flex">
         <input type="file" id="product_image1" name="product_image1" class="form-control w-90 m-auto" required="required">
-        <img src="./product_images/apple.jpg" alt="" class="product_img">
+        <img src="./product_images/<?php echo $product_image1 ?>" alt="" class="product_img">
       </div>
     </div>
 
@@ -52,7 +114,7 @@
       <label for="product_image2" class=" form-label">Product Image2</label>
       <div class="d-flex">
         <input type="file" id="product_image1" name="product_image1" class="form-control w-90 m-auto" required="required">
-        <img src="./product_images/book.jpg" alt="" class="product_img">
+        <img src="./product_images/<?php echo $product_image2 ?>" alt="" class=" product_img">
       </div>
     </div>
 
@@ -61,13 +123,13 @@
       <label for="product_image3" class=" form-label">Product Image3</label>
       <div class="d-flex">
         <input type="file" id="product_image3" name="product_image3" class="form-control w-90 m-auto" required="required">
-        <img src="./product_images/book2.jpg" alt="" class="product_img">
+        <img src="./product_images/<?php echo $product_image3 ?>" alt="" class=" product_img">
       </div>
     </div>
 
     <div class="form-outline w-50 m-auto mb-4">
       <label for="product_price" class=" form-label">Product Price</label>
-      <input type="text" id="product_price" name="product_price" class="form-control" required="required">
+      <input type="text" id="product_price" name="product_price" class="form-control" required="required" value="<?php echo $product_price ?>">
     </div>
 
     <div class="W-50 m-auto">
